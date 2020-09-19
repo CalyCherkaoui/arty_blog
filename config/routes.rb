f60bd5fb85f08d1bff2307d59cd842b3f38c442d
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-19 15:50:09 -0500

    adding routes articles under the scope of authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "staticpages#home"
   devise_for :authors
-  resources :articles
-  
+  # resources :articles
+
+  scope module: 'authors' do
+    resources :articles
+  end
 end
