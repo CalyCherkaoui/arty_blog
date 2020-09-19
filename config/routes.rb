71043153cb59c6fdd598421ce6344004ac97df4f
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-19 13:57:48 -0500

    Creating Author a Devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "staticpages#home"
 end
